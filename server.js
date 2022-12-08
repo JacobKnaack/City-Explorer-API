16238fb926f739bf80fcd0abc57b57d6c0842ac2
JacobKnaack <jacobknck@gmail.com>
2022-12-08 12:25:24 -0800

    updates weather route,  adds weather file for refactoring

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const mockWeatherData = require('./data/weather.json');
+const weather = require('./weather.js');
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const PORT = process.env.PORT;
 
 // singleton
 const app = express(); // what type of thing is app?
 app.use(cors());
+
+// app.get('/weather', weather);
 
 app.get('/weather', async (request, response, next) => {
 
@@ -29,17 +33,28 @@ app.get('/weather', async (request, response, next) => {
       method: "GET",
       url: url,
     });
-    let weatherData = weatherResponse.data.data;
+    let weatherData = weatherResponse.data.data.map((weather) => { return new Forecast(weather) });
     response.send(weatherData);
   } catch (e) {
-    response.status(400).send('Bad Request');
-  }
+    console.log(e.response);
 
+    if (e.response.data.status_code === 429) {
+      let weatherData = mockWeatherData[0].data.map((weather) => {
+        return new Forecast(weather);
+      });
+      response.status(200).send(weatherData);
+    } else {
+      response.status(400).send({message: 'Bad Request', error: e});
+    }
+  }
 });
 
+// create an object with data and description.
 class Forecast {
   constructor(weather) {
     // attach to Forecast object as necessary
+    this.date = weather.datetime;
+    this.description = weather.weather.description;
   }
 }
 
---
+++ weather.js
@@ -0,0 +1,7 @@
+'use strict';
+
+async function weather(request, response, next) {
+  // do all the same things that you were doing before
+}
+
+module.exports = weather;
